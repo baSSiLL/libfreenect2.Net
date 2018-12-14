0ddb0b9151a1458da9d0744c712225ee91441b75
Vasili Maslov <bassill@yandex.ru>
2018-12-14 21:35:44 +0300

    Tests on camera parameters.

---
+++ libfreenect2.Net.Test/CameraParametersTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace libfreenect2Net.Test
+{
+    [TestClass]
+    public class CameraParametersTests : TestClassWithContext
+    {
+        [TestMethod]
+        public void InfraRedParamsShouldHaveReasonableValues()
+        {
+            using (var device = Context.OpenDefaultDevice())
+            {
+                if (device != null)
+                {
+                    device.StartAll();
+                    var p = device.InfraRedCameraParameters;
+                    var fovXDegrees = 2 * Math.Atan2(256, p.FocalLengthX) * 180 / Math.PI;
+                    var fovYDegrees = 2 * Math.Atan2(212, p.FocalLengthY) * 180 / Math.PI;
+                    device.Stop();
+
+                    fovXDegrees.Should().BeApproximately(70, 1);
+                    fovYDegrees.Should().BeApproximately(60, 1);
+                    p.PrincipalPointX.Should().BeApproximately(256, 10);
+                    p.PrincipalPointY.Should().BeApproximately(212, 10);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ColorParamsShouldHaveReasonableValues()
+        {
+            using (var device = Context.OpenDefaultDevice())
+            {
+                if (device != null)
+                {
+                    device.StartAll();
+                    var p = device.ColorCameraParameters;
+                    var fovXDegrees = 2 * Math.Atan2(960, p.FocalLengthX) * 180 / Math.PI;
+                    var fovYDegrees = 2 * Math.Atan2(540, p.FocalLengthY) * 180 / Math.PI;
+                    device.Stop();
+
+                    fovXDegrees.Should().BeApproximately(83.5, 1);
+                    fovYDegrees.Should().BeApproximately(53.5, 1);
+                    p.PrincipalPointX.Should().BeApproximately(960, 15);
+                    p.PrincipalPointY.Should().BeApproximately(540, 15);
+                }
+            }
+        }
+    }
+}
